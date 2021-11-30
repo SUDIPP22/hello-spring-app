e76efd071bed1b094366503b20ac4f4902ee1c8e
Sudip Panja <panja370@gmail.com>
2021-12-01 03:10:14 +0530

    Added : Code for invoking the html page by using Thymeleaf

--- src/main/java/com/bridgelabz/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/controller/HelloWebController.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Purpose : To invoke a simple HTML page by using MVC Controller
+ *
  * @author SUDIP PANJA
  * @version : 0.0.1-SNAPSHOT
  * @since 2021-12-01
@@ -15,5 +17,12 @@ public class HelloWebController {
     @GetMapping("/web")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message",
+                "This is a custom message : Hello From BridgeLabz");
+        return "message";
     }
 }
