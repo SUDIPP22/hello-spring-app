9cad770517204e6295c41973c4b262393b99db45
Sudip Panja <panja370@gmail.com>
2021-12-01 01:49:42 +0530

    Added : Documentation comments for the class level

--- src/main/java/com/bridgelabz/MyFirstSpringAppApplication.java
+++ src/main/java/com/bridgelabz/MyFirstSpringAppApplication.java
@@ -3,12 +3,20 @@ package com.bridgelabz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Purpose : To simulate simple hello world message
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-01
+ */
 @SpringBootApplication
 public class MyFirstSpringAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyFirstSpringAppApplication.class, args);
-		System.out.println("Hello World!!!");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringAppApplication.class, args);
+        System.out.println("Hello World!!!");
+    }
 
 }
