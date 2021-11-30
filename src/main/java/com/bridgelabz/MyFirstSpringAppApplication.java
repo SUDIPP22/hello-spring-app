0d401bbacd070cc09771914d7425542b7bc3be2b
Sudip Panja <panja370@gmail.com>
2021-12-01 01:40:08 +0530

    Added : MyFirstSpringAppApplication class to display a simple Hello World by using spring boot application

---
+++ src/main/java/com/bridgelabz/MyFirstSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringAppApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
