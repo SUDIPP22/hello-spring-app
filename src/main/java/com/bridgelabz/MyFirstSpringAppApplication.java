4ad9ad652aecbae517ad35a315099568f88dc79c
Sudip Panja <panja370@gmail.com>
2021-12-02 16:35:31 +0530

    Added : Simple logging framework by using built in SLF4J

--- src/main/java/com/bridgelabz/MyFirstSpringAppApplication.java
+++ src/main/java/com/bridgelabz/MyFirstSpringAppApplication.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
+import com.bridgelabz.component.DemoBean;
+import com.bridgelabz.controller.HelloRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 /**
@@ -13,10 +18,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MyFirstSpringAppApplication {
+    public static final Logger logger = LoggerFactory.getLogger(MyFirstSpringAppApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MyFirstSpringAppApplication.class, args);
-        System.out.println("Hello World!!!");
+        logger.debug("Hello World!!!");
+        ApplicationContext context = SpringApplication.run(MyFirstSpringAppApplication.class, args);
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        logger.warn("Demo Bean = " + demoBean);
+        logger.warn(String.valueOf(context.getBean(HelloRestController.class)));
     }
 
 }
---
+++ src/main/java/com/bridgelabz/component/DemoBean.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Purpose : To simulate Logger using DemoBean class
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-02
+ */
+@Component
+public class DemoBean {
+    public DemoBean() {
+    }
+}
